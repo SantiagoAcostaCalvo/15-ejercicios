1d0f2836ccaa3dd14a6a73f8854f71e171d6b6fa
SantiagoAcostaCalvo <140684133+SantiagoAcostaCalvo@users.noreply.github.com>
2025-02-26 23:28:19 -0500

    Update PrincipalBiblioteca.java

--- Biblioteca/PrincipalBiblioteca.java
+++ Biblioteca/PrincipalBiblioteca.java
@@ -2,12 +2,10 @@ package Biblioteca;
 
 public class PrincipalBiblioteca {
     public static void main(String[] args) {
-        PrestamoLibro prestamoLibro = new PrestamoLibro();
-        prestamoLibro.titulo = "Cien años de soledad";
-        prestamoLibro.autor = "Gabriel García Márquez";
-        prestamoLibro.usuario = "María González";
-        prestamoLibro.fechaDevolucion = "10/02/2025";
+        PrestamoLibro prestamoLibro = new PrestamoLibro("Cien años de soledad", "Gabriel García Márquez", "María González", "10/02/2025");
         System.out.println(prestamoLibro.mostrarMensaje());
-    }
 
+        PrestamoDigital prestamoDigital = new PrestamoDigital("1984", "George Orwell", "Carlos Pérez", "15/03/2025", "PDF");
+        System.out.println(prestamoDigital.mostrarMensaje());
+    }
 }
