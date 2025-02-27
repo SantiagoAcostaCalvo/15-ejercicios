57fd66e3abbdce1c7106b200d9a16e03f330b8e1
SantiagoAcostaCalvo <140684133+SantiagoAcostaCalvo@users.noreply.github.com>
2025-02-26 23:33:51 -0500

    Update PersonaHotel.java

--- HotelPersona/PersonaHotel.java
+++ HotelPersona/PersonaHotel.java
@@ -9,7 +9,45 @@ public class PersonaHotel {
         this.diasReserva = diasReserva;
     }
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getDiasReserva() {
+        return diasReserva;
+    }
+
+    public void setDiasReserva(int diasReserva) {
+        this.diasReserva = diasReserva;
+    }
+
     public String getInfo() {
         return "Huésped: " + nombre + " | Días de reserva: " + diasReserva;
+    }
+}
+
+class ClienteVIP extends PersonaHotel {
+    private String beneficios;
+
+    public ClienteVIP(String nombre, int diasReserva, String beneficios) {
+        super(nombre, diasReserva);
+        this.beneficios = beneficios;
+    }
+
+    public String getBeneficios() {
+        return beneficios;
+    }
+
+    public void setBeneficios(String beneficios) {
+        this.beneficios = beneficios;
+    }
+
+    @Override
+    public String getInfo() {
+        return super.getInfo() + " | Beneficios: " + beneficios;
     }
 }
