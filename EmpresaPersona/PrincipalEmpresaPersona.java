9f586de7e38bc08c73ac8017f7842fcdfd3264d4
SantiagoAcostaCalvo <140684133+SantiagoAcostaCalvo@users.noreply.github.com>
2025-02-26 23:30:51 -0500

    Update PrincipalEmpresaPersona.java

--- EmpresaPersona/PrincipalEmpresaPersona.java
+++ EmpresaPersona/PrincipalEmpresaPersona.java
@@ -4,5 +4,8 @@ public class PrincipalEmpresaPersona {
     public static void main(String[] args) {
         PersonaEmpresa p = new PersonaEmpresa("Ana Martínez", "Gerente de Ventas");
         System.out.println(p.getInfo());
+
+        Gerente g = new Gerente("Carlos Pérez", "Director General", 5000.00);
+        System.out.println(g.getInfo());
     }
 }
