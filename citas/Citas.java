e2e4d97c54ff8b3229e7635f6c0290248b2f98c9
SantiagoAcostaCalvo <140684133+SantiagoAcostaCalvo@users.noreply.github.com>
2025-02-26 23:35:36 -0500

    Update Citas.java

--- citas/Citas.java
+++ citas/Citas.java
@@ -1,11 +1,73 @@
 package citas;
 
 public class Citas {
-    public String nombre;
-    public String descripcion;
-    public String consultorio;
-    public String hora;
-    public String mostrarMensaje(){
-        return nombre+" "+consultorio+" "+hora+"su cita ha sido registrada";
+    private String nombre;
+    private String descripcion;
+    private String consultorio;
+    private String hora;
+
+    public Citas(String nombre, String descripcion, String consultorio, String hora) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.consultorio = consultorio;
+        this.hora = hora;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getConsultorio() {
+        return consultorio;
+    }
+
+    public void setConsultorio(String consultorio) {
+        this.consultorio = consultorio;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    public String mostrarMensaje() {
+        return nombre + " " + consultorio + " " + hora + " su cita ha sido registrada";
+    }
+}
+
+class CitaEspecialista extends Citas {
+    private String especialidad;
+
+    public CitaEspecialista(String nombre, String descripcion, String consultorio, String hora, String especialidad) {
+        super(nombre, descripcion, consultorio, hora);
+        this.especialidad = especialidad;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    @Override
+    public String mostrarMensaje() {
+        return super.mostrarMensaje() + " con el especialista en " + especialidad;
     }
 }
