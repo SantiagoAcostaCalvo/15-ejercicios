75d03abe50d013db68ba74eec3ea2cb49499a2d8
SantiagoAcostaCalvo <140684133+SantiagoAcostaCalvo@users.noreply.github.com>
2025-02-26 23:32:36 -0500

    Update PrincipalPersonaEquipo.java

--- EquipoPersona/PrincipalPersonaEquipo.java
+++ EquipoPersona/PrincipalPersonaEquipo.java
@@ -4,6 +4,8 @@ public class PrincipalPersonaEquipo {
     public static void main(String[] args) {
         PersonaEquipo p = new PersonaEquipo("Lionel Messi", "Delantero");
         System.out.println(p.getInfo());
+
+        Entrenador e = new Entrenador("Pep Guardiola", "Director Técnico", 20);
+        System.out.println(e.getInfo());
     }
 }
-
