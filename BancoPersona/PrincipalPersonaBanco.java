60f874895c17b1099b8335a7e655acc867202152
SantiagoAcostaCalvo <140684133+SantiagoAcostaCalvo@users.noreply.github.com>
2025-02-26 23:26:03 -0500

    Update PrincipalPersonaBanco.java

--- BancoPersona/PrincipalPersonaBanco.java
+++ BancoPersona/PrincipalPersonaBanco.java
@@ -4,5 +4,8 @@ public class PrincipalPersonaBanco {
     public static void main(String[] args) {
         PersonaBanco p = new PersonaBanco("Carlos López", "12345678");
         System.out.println(p.getInfo());
+
+        ClienteBanco c = new ClienteBanco("Ana Pérez", "87654321", 1500.50);
+        System.out.println(c.getInfo());
     }
 }
