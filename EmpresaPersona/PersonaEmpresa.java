96f0cb8f9ae32df0ed22cb5443d371308777ae06
SantiagoAcostaCalvo <140684133+SantiagoAcostaCalvo@users.noreply.github.com>
2025-02-26 23:29:57 -0500

    Update PersonaEmpresa.java

--- EmpresaPersona/PersonaEmpresa.java
+++ EmpresaPersona/PersonaEmpresa.java
@@ -1,4 +1,5 @@
 package EmpresaPersona;
+
 public class PersonaEmpresa {
     private String nombre;
     private String cargo;
@@ -8,7 +9,45 @@ public class PersonaEmpresa {
         this.cargo = cargo;
     }
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
     public String getInfo() {
         return "Empleado: " + nombre + " | Cargo: " + cargo;
+    }
+}
+
+class Gerente extends PersonaEmpresa {
+    private double salario;
+
+    public Gerente(String nombre, String cargo, double salario) {
+        super(nombre, cargo);
+        this.salario = salario;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    @Override
+    public String getInfo() {
+        return super.getInfo() + " | Salario: " + salario;
     }
 }
