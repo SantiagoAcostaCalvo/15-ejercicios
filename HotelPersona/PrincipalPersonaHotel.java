73ffa19df3a4b290cb8a3c2dca2ca37473c90062
SantiagoAcostaCalvo <140684133+SantiagoAcostaCalvo@users.noreply.github.com>
2025-02-26 23:34:37 -0500

    Update PrincipalPersonaHotel.java

--- HotelPersona/PrincipalPersonaHotel.java
+++ HotelPersona/PrincipalPersonaHotel.java
@@ -4,5 +4,8 @@ public class PrincipalPersonaHotel {
     public static void main(String[] args) {
         PersonaHotel p = new PersonaHotel("Mario Gómez", 5);
         System.out.println(p.getInfo());
+
+        ClienteVIP vip = new ClienteVIP("Laura Martínez", 7, "Desayuno gratis, acceso al spa");
+        System.out.println(vip.getInfo());
     }
 }
