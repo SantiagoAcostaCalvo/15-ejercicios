75fe64a2593db80089eec3310b6fd220542e9771
SantiagoAcostaCalvo <140684133+SantiagoAcostaCalvo@users.noreply.github.com>
2025-02-26 23:24:58 -0500

    Update PersonaBanco.java

--- BancoPersona/PersonaBanco.java
+++ BancoPersona/PersonaBanco.java
@@ -9,7 +9,45 @@ public class PersonaBanco {
         this.identificacion = identificacion;
     }
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getIdentificacion() {
+        return identificacion;
+    }
+
+    public void setIdentificacion(String identificacion) {
+        this.identificacion = identificacion;
+    }
+
     public String getInfo() {
         return "Cliente: " + nombre + " | ID: " + identificacion;
+    }
+}
+
+class ClienteBanco extends PersonaBanco {
+    private double saldo;
+
+    public ClienteBanco(String nombre, String identificacion, double saldo) {
+        super(nombre, identificacion);
+        this.saldo = saldo;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    @Override
+    public String getInfo() {
+        return super.getInfo() + " | Saldo: " + saldo;
     }
 }
