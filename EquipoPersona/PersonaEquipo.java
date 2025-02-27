bde2a458d03eb2be08b3e9f6fd5d98765c9a5962
SantiagoAcostaCalvo <140684133+SantiagoAcostaCalvo@users.noreply.github.com>
2025-02-26 23:31:50 -0500

    Update PersonaEquipo.java

--- EquipoPersona/PersonaEquipo.java
+++ EquipoPersona/PersonaEquipo.java
@@ -9,7 +9,45 @@ public class PersonaEquipo {
         this.posicion = posicion;
     }
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(String posicion) {
+        this.posicion = posicion;
+    }
+
     public String getInfo() {
         return "Jugador: " + nombre + " | Posición: " + posicion;
+    }
+}
+
+class Entrenador extends PersonaEquipo {
+    private int experiencia;
+
+    public Entrenador(String nombre, String posicion, int experiencia) {
+        super(nombre, posicion);
+        this.experiencia = experiencia;
+    }
+
+    public int getExperiencia() {
+        return experiencia;
+    }
+
+    public void setExperiencia(int experiencia) {
+        this.experiencia = experiencia;
+    }
+
+    @Override
+    public String getInfo() {
+        return super.getInfo() + " | Años de experiencia: " + experiencia;
     }
 }
